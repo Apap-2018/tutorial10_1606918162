eb31bc5e08a0816f1290ebc6043168dbe1a14602
Arief Fadli Wicaksono <arieffadliwicaksono@gmail.com>
2018-12-05 15:31:07 +0700

    first commit, belum latihan

---
+++ src/utils/Appointment.js
@@ -0,0 +1,42 @@
+const cors = "https://cors-anywhere.herokuapp.com/";
+const baseUrl = "http://si-appointment.herokuapp.com/api";
+
+export const Appointment = {
+	getAllPasien() {
+		return fetch(`${cors}${baseUrl}/1/getAllPasien`, {
+			method: 'GET',
+		})
+		.then(response => {
+			return response.json()
+		})
+		.then(jsonResponse => {
+			return jsonResponse
+		})
+	},
+	getDetailPasien(idPasien) {
+		return fetch(`${cors}${baseUrl}/getPasien/${idPasien}`, {
+			method: 'GET',
+		})
+		.then(response => {
+			return response.json()
+		})
+		.then(jsonResponse => {
+			return jsonResponse
+		})
+	},
+	updateStatusPasien(requestBody) {
+		return fetch(`${cors}${baseUrl}/1/updatePasien`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		})
+		.then(response => {
+			return response.json()
+		})
+		.then(jsonResponse => {
+			return jsonResponse
+		})
+	}
+}
